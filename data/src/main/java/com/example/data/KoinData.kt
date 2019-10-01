74586a12a9e4b5610289f9751adf8bfbe5281b51
Junjie <jjtommyliii@gmail.com>
2019-10-01 13:34:16 +0200

    Nuevo firebase

--- data/src/main/java/com/example/data/KoinData.kt
+++ data/src/main/java/com/example/data/KoinData.kt
@@ -7,7 +7,7 @@ import org.koin.dsl.module.module
 class KoinData {
     val dataModule by lazy {
         module{
-            single<LoginRepository>{ LoginRepositoryImp()}
+           single<LoginRepository>{ LoginRepositoryImp()}
         }
     }
 }
