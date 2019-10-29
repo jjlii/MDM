905871a6467a7ee5d2f6522ee3ce9c6f440ada6e
jj <jjtommyliii@gmail.com>
2019-10-29 13:15:09 +0100

    Quitar Tool Bar. Empieza el cambio del Bottom Nav Bar

--- app/src/main/java/com/example/mdm_everis/MainActivity.kt
+++ app/src/main/java/com/example/mdm_everis/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.mdm_everis
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,16 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() = findNavController( R.id.nav_host_fragment).navigateUp()
 
-    fun showToolbar(show: Boolean) {
-        if (show) {
-            main_toolbar.visibility = View.VISIBLE
-            setSupportActionBar(main_toolbar)
-            main_toolbar.setNavigationOnClickListener { onBackPressed() }
-        } else {
-            main_toolbar.visibility = View.GONE
-            supportActionBar?.hide()
-        }
-    }
 
 
     fun setTitle(title: String){ supportActionBar?.title = title }
