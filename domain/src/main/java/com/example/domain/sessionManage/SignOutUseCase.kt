d63487718c40cfab764dd14c6da9d35b9093b652
jj <jjtommyliii@gmail.com>
2019-11-27 17:09:03 +0100

    Reorganizar proyecto

--- domain/src/main/java/com/example/domain/sessionManage/SignOutUseCase.kt
+++ domain/src/main/java/com/example/domain/sessionManage/SignOutUseCase.kt
@@ -3,6 +3,7 @@ package com.example.domain.sessionManage
 import com.example.core.Either
 import com.example.core.UseCase
 import com.example.core.failure.Failure
+import com.example.domain.repository.SessionManageRepository
 
 class SignOutUseCase(private val sessionManageRepository: SessionManageRepository): UseCase<Failure,Boolean,Unit>() {
 
